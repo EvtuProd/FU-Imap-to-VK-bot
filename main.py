122fc0c7299ac11b7547e0710cfc53d6f578341b
EvtuProd <mail@tuhachewscky.ru>
2024-05-05 00:28:05 +0300

    Добавлено отображение тела письма

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def decode_base64(encoded_string):
 
 def process_emails():
     # Подключаемся к серверу почты
-
+    mail = imaplib.IMAP4_SSL('imap.yandex.ru')
+    mail.login('tuhachewscky@yandex.ru','nivlqlsojzdjepyv')
 
     # Выбираем ящик с почтой (inbox)
     mail.select('inbox')
@@ -42,6 +43,19 @@ def process_emails():
         # Теперь можно обработать письмо
         # Например, распечатать его заголовок
         print('From:', sender_name, ':', sender_email, 'Subject:', email_message['Subject'])
+
+        # Печатаем текст письма, если он есть
+        if email_message.is_multipart():
+            for part in email_message.walk():
+                content_type = part.get_content_type()
+                content_disposition = str(part.get("Content-Disposition"))
+
+                # Ищем текстовую часть письма
+                if "text/plain" in content_type:
+                    # Декодируем и печатаем текст
+                    body = part.get_payload(decode=True).decode()
+                    print("Message Body:", body)
+
 
     # Закрываем соединение
     mail.close()
